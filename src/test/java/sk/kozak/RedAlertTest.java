d46ea619e20b8bebcf71ff53b0a7d6344ed97147
ataman329 <adrian.kozak89@gmail.com>
2025-06-14 12:12:55 +0200

    najnovsie commity

---
+++ src/test/java/sk/kozak/RedAlertTest.java
@@ -0,0 +1,54 @@
+package sk.kozak;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class RedAlertTest {
+
+    private WebDriver driver; // class variable
+    private static final String BASE_URL = "http://localhost/playground/redalert.php";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() {
+        driver.get(BASE_URL);
+
+//        driver.findElement(By.id("alert1")).click();
+//        Alert alert = driver.switchTo().alert();
+//        alert.accept();
+//        System.out.println(driver.findElement(By.xpath("//div[@class='result']/h1")).getText());
+
+//        driver.findElement(By.id("alert2")).click();
+//        Alert alert = driver.switchTo().alert();
+//        System.out.println(alert.getText());
+//        Assert.assertEquals("Second alert!", alert.getText());
+//        alert.dismiss();
+//        Assert.assertEquals("Negative", driver.findElement(By.xpath("//div[@class='result']/h1")).getText());
+//        System.out.println(driver.findElement(By.xpath("//div[@class='result']/h1")).getText());
+
+        driver.findElement(By.id("alert3")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("Yuri");
+        alert.accept();
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='result']/h1")).getText().contains("Yuri"));
+        }
+
+
+    @After
+    public void tearDown() {
+    }
+}
---
+++ src/test/java/sk/kozak/Tabulkav2Test.java
@@ -0,0 +1,42 @@
+package sk.kozak;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Tabulkav2Test {
+
+    private WebDriver driver; // class variable
+    private static final String BASE_URL = "http://localhost/playground/tabulka.php";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() {
+        driver.get(BASE_URL);
+        List<WebElement> rows = driver.findElements(By.xpath("//table/tbody/tr"));
+//        System.out.println(rows);
+        for (WebElement row : rows) {
+//            System.out.println(row.findElement(By.xpath("./td[3]")).getText());
+            Assert.assertFalse(row.findElement(By.xpath("./td[3]")).getText().isEmpty());
+
+        }
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+        }
+    }
+}
