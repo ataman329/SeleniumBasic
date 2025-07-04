025d0f2e7abd855e2e20fa1a5ca777148a15b747
ataman329 <adrian.kozak89@gmail.com>
2025-07-04 10:43:44 +0200

    refactoring kodu, happy/unhappy paths

---
+++ src/test/java/sk/kozak/KalkulackaTest.java
@@ -0,0 +1,82 @@
+package sk.kozak;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class KalkulackaTest {
+    private WebDriver driver; // class variable
+    private static final String BASE_URL = "http://localhost/playground/kalkulacka.php";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @Test
+    public void testSum() {
+        checkSum("7", "3", "10");
+        checkSum("50", "500", "550");
+        checkSum("-5", "5", "0");
+        checkSum("10", "99", "109");
+        checkSum("85", "30", "115");
+        checkSum("-25", "30", "5");
+    }
+
+    @Test public void testDeduct() {
+        checkDeduct("5", "2", "3");
+        checkDeduct("7", "3", "4");
+        checkDeduct("1000", "8", "992");
+    }
+
+    @Test public void testReset(){
+        enterInputs("5", "6");
+        driver.findElement(By.id("count")).click();
+        driver.findElement(By.id("reset")).click();
+
+        Assert.assertTrue(driver.findElement(By.id("firstInput")).getAttribute("value").isEmpty());
+        Assert.assertTrue(driver.findElement(By.id("secondInput")).getAttribute("value").isEmpty());
+        Assert.assertTrue(driver.findElement(By.id("result")).getText().isEmpty());
+    }
+
+    @Test public void testInvalidInputs(){
+        enterInputs("blud1", "blud2");
+        driver.findElement(By.id("count")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[input[@id='firstInput']]"))
+                .getAttribute("class").contains("has-error"));
+        Assert.assertTrue(driver.findElement(By.xpath("//div[input[@id='secondInput']]"))
+                .getAttribute("class").contains("has-error"));
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    private void enterInputs(String firstInput, String secondInput) {
+        driver.findElement(By.id("firstInput")).clear();
+        driver.findElement(By.id("firstInput")).sendKeys(firstInput);
+        driver.findElement(By.id("secondInput")).clear();
+        driver.findElement(By.id("secondInput")).sendKeys(secondInput);
+    }
+
+    private void checkSum(String firstInput, String secondInput, String expected) {
+        enterInputs(firstInput, secondInput);
+        driver.findElement(By.id("count")).click();
+        Assert.assertEquals(expected, driver.findElement(By.id("result")).getText());
+    }
+
+    private void checkDeduct(String firstInput, String secondInput, String expected) {
+        enterInputs(firstInput, secondInput);
+        driver.findElement(By.id("deduct")).click();
+        Assert.assertEquals(expected, driver.findElement(By.id("result")).getText());
+    }
+}
